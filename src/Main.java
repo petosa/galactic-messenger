1bbb4c156d75b85242de164dd4001b93f0279e0f
William Ma <willzma15@gmail.com>
2016-06-11 01:50:00 -0400

    Port forwarding eh

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
+import java.net.ServerSocket;
+
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main extends Application {
 
+    private static Scanner sc = new Scanner(System.in);
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
@@ -16,6 +24,38 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(51012);
+        } catch (IOException e) {
+            System.out.print("fuk u faguette | ");
+            e.printStackTrace();
+        }
+
+        System.out.println("The port we will use is " + server.getLocalPort());
+        System.out.println("Begin inputting messages you want to send");
+
+        while (true) {
+            try {
+                Socket serverReader = server.accept();
+                DataInputStream in =
+                        new DataInputStream(serverReader.getInputStream());
+
+                System.out.println("Just connected to "
+                        + serverReader.getRemoteSocketAddress());
+
+                System.out.println(in.readUTF());
+
+                DataOutputStream out =
+                        new DataOutputStream(serverReader.getOutputStream());
+                out.writeUTF("Fuckinggay says " + sc.nextLine());
+                serverReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
     }
 }
