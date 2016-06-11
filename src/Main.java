529f1f4f682927badcd7d21d807e78161392980c
Nick Peto <nick.petosa@gmail.com>
2016-06-11 01:50:00 -0400

    Comments

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
@@ -16,6 +19,31 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+
+        //launch(args);
     }
+
+            /*final String SERVERNAME = "theunixphilosophy.com";
+        int PORT = 51012;
+        try
+        {
+            System.out.println("Connecting to " + SERVERNAME +
+                    " on port " + PORT);
+            Socket client = new Socket(SERVERNAME, PORT);
+            System.out.println("Just connected to "
+                    + client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+            out.writeUTF("Hello from "
+                    + client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in =
+                    new DataInputStream(inFromServer);
+            System.out.println("Server says " + in.readUTF());
+            client.close();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }*/
+
 }
